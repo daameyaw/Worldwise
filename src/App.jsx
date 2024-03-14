6b656d978333e9fa6ab3848e55be09b954c5b812
daameyaw <davidameyaw607@gmail.com>
2024-03-13 20:20:40 -0800

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
 function App() {
-  return <div>World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="app" element={<AppLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
