a56845799f9739669a7d66c79d4a5676a191c40c
daameyaw <davidameyaw607@gmail.com>
2024-04-05 13:02:39 -0800

    cds

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,12 @@
+import styles from "./CityItem.module.css";
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+  return (
+    <li className={styles.CityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 cl></h3>
+    </li>
+  );
+}
+
+export default CityItem;
