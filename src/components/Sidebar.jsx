a963ba574e7f7468a06e099f0c3bef4faf2bafb3
daameyaw <davidameyaw607@gmail.com>
2024-04-15 21:05:21 -0800

    cds

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import styles from "./Sidebar.module.css";
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <Logo />
+      {/* <Logo /> */}
       <AppNav />
 
       <Outlet />
