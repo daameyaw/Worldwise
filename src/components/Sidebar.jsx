4400498946bbdf439fdbbce22f29e8939e41da73
daameyaw <davidameyaw607@gmail.com>
2024-04-16 06:44:25 -0800

    cds

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,12 +10,12 @@ function Sidebar() {
 
       <Outlet />
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc. All
           rights reserved
         </p>
-      </footer>
+      </footer> */}
     </div>
   );
 }
