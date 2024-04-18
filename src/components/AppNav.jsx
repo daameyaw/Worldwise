937216921475e75b10cebb9fb5cfb46e5163e13a
daameyaw <davidameyaw607@gmail.com>
2024-04-17 21:58:35 -0800

    cds

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
-// function AppNav() {
-//   return (
-//     <ul className={styles.nav}>
-//       <li>
-//         <NavLink to="cities">Cities</NavLink>
-//       </li>
-//       <li>
-//         <NavLink to="countries">Countries</NavLink>
-//       </li>
-//     </ul>
-//   );
-// }
+function AppNav() {
+  return (
+    <ul className={styles.nav}>
+      <li>
+        <NavLink to="cities">Cities</NavLink>
+      </li>
+      <li>
+        <NavLink to="countries">Countries</NavLink>
+      </li>
+    </ul>
+  );
+}
 
 export default AppNav;
