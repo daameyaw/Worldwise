375bed2cff5ac053d70c6936bc00fb47659e98ae
daameyaw <davidameyaw607@gmail.com>
2024-04-23 15:31:10 -0800

    cds

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -9,11 +9,14 @@ function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
   return (
-    <ul className={styles.cityList}>
-      {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
-      ))}
-    </ul>
+    <>
+      <div>Hello</div>
+      <ul className={styles.cityList}>
+        {cities.map((city) => (
+          <CityItem city={city} key={city.id} />
+        ))}
+      </ul>
+    </>
   );
 }
 
