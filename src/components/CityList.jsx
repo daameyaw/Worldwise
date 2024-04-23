bd7c066faa07a508f7768a84a8f15625bc1de74a
daameyaw <davidameyaw607@gmail.com>
2024-04-22 20:11:19 -0800

    cds

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -6,7 +6,7 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 
 function CityList({ cities, isLoading }) {
-  // if (isLoading) return <Spinner />;
+  if (isLoading) return <Spinner />;
 
   return (
     <ul className={styles.cityList}>
